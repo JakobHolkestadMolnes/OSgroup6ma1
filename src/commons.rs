54e283376f1b40e822d5f1bc76ac11cc815c92e1
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2023-02-07 11:26:17 +0100

    refactor: :recycle: replace push str with raw strings

--- src/commons.rs
+++ src/commons.rs
@@ -9,28 +9,30 @@ pub trait Server {
 }
 
 fn html_body() -> (i64, String) {
-    let mut body = String::new();
-    body.push_str("<!DOCTYPE html>");
-    body.push_str("<html>");
-    body.push_str("<head>");
-    body.push_str("<title>My first Rust web server</title>");
-    body.push_str("</head>");
-    body.push_str("<body>");
-    body.push_str("<h1>Hello, world!</h1>");
-    body.push_str("</body>");
-    body.push_str("</html>");
+    let body = String::from(
+        r#"<!DOCTYPE html>
+        <html>
+        <head>
+        <title>My first Rust web server</title>
+        </head>
+        <body>
+        <h1>Hello, world!</h1>
+        </body>
+        </html>"#,
+    );
     let content_length = body.len() as i64;
     (content_length, body)
 }
 
-pub fn html_response(content_length: i64, body: String) -> String {
-    let mut html = String::new();
-    html.push_str("HTTP/1.1 200 OK\r");
-    html.push_str("Content-Type: text/html; charset=UTF-8\r\n");
-    html.push_str(&format!("Content-Length: {}\r", content_length));
-    html.push_str("\n\r\n");
-    html.push_str(&body);
-    html
+pub fn html_response(content_length: i64, body: &str) -> String {
+    format!(
+        r#"HTTP/1.1 200 OK
+        Content-Type: text/html; charset=UTF-8
+        Content-Length: {}
+
+        {}"#,
+        content_length, body
+    )
 }
 
 /// Simulate a heavy computation, sleeps for 2 seconds.
@@ -45,7 +47,7 @@ pub fn handle_client(mut stream: TcpStream) {
     heavy_computation();
 
     let (content_length, body) = html_body();
-    let html = html_response(content_length, body);
+    let html = html_response(content_length, &body);
     let _ = stream.write(html.as_bytes());
     let _ = stream.flush();
 }
